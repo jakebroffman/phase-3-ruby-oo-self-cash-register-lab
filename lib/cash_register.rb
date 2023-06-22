3a3f0292eb1a447ec189d0a55a694fe25935f45f
jakebroffman <jake.broffman1088@gmail.com>
2023-06-22 06:08:40 -0400

    commit tree 01e764dce7743d251ecfcc222646b4f9d3bbab66

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
 
+class CashRegister
+
+    attr_accessor :total, :dicount, :items, :last_transaction 
+
+    def initialize(discount = 0)
+        @discount = discount
+        @total = 0
+        @items = []
+    end
+
+    def add_item(title, price)
+        @total += price
+        @items << title
+    end
+
+    def total
+        @total
+    end
+
+    def discount
+        @discount
+    end
+
+end
